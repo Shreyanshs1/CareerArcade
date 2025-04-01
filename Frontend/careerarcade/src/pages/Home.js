cf1b9b7bc468938a0afd6e3585c554d3a128993b
Shreyansh Srivastava <shreyanshsri1807@gmail.com>
2025-04-01 12:07:17 +0530

    Add routing based on role in home page

--- Frontend/careerarcade/src/pages/Home.js
+++ Frontend/careerarcade/src/pages/Home.js
@@ -1,6 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom';
 
 function Home() {
+  const navigate = useNavigate(); //React hook for navigate
+  // Check if user is already logged in and redirect
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        const role = localStorage.getItem("role");
+        const tokenExp = localStorage.getItem("tokenExp");
+        const currentTime = Date.now();
+
+        if (token && role && tokenExp) {
+        if (currentTime < tokenExp) {
+            // Redirect to respective dashboard based on role
+            switch (role) {
+            case "Admin":
+                navigate("/admin/dashboard");
+                break;
+            case "Employer":
+                navigate("/employer/dashboard");
+                break;
+            case "Jobseeker":
+                navigate("/jobseeker/dashboard");
+                break;
+            default:
+                navigate("/");
+            }
+        }
+        }
+    }, [navigate]);
   return (
     <div>Home</div>
   )
